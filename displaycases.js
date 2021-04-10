845d66ef1d9d768a649d1f7f0ffacf9af884e322
Shyam-Makwana <makwanashyam6@gmail.com>
2021-04-10 23:19:25 +0530

    Displaying the Cases on Map

---
+++ displaycases.js
@@ -0,0 +1,35 @@
+const updateMap = () => {
+    fetch("/coronacases.json")
+        .then(response => response.json())
+        .then(rsp => {
+            rsp.data.forEach(element => {
+                var el = document.createElement('div');
+                el.className = 'marker';
+
+                var latitude = element.latitude;
+                var longitude = element.longitude;
+                var cases = element.infected;
+                let color;
+                if (cases > 255) color = `rgb(255,0,0)`;
+                else color = `rgb(${cases},0,0)`;
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                })
+                .setLngLat([longitude, latitude])
+                .setPopup(
+                    new mapboxgl.Popup({ offset: 25 }) // add popups
+                        .setHTML(
+                            '<h3>' +
+                            'Corona Cases ' +
+                            '</h3><p>' +
+                            'to Display ' +
+                            '</p>'
+                        )
+                )
+                .addTo(map);
+            });
+        })
+}
+
+updateMap();
