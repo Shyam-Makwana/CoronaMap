dfdb788451d5d09be171730ae22c72c2544d21df
Shyam-Makwana <makwanashyam6@gmail.com>
2021-04-11 10:34:00 +0530

    Added Popup to display Cases

--- displaycases.js
+++ displaycases.js
@@ -9742,31 +9742,41 @@ let data = [
     }
 ]
 
+const addCommas = number => {
+    number += '';
+    var x = number.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
+
 const updateMap = () => {
     data.forEach(element => {
         var el = document.createElement('div');
         el.className = 'marker';
 
-        var latitude = element.latitude;
-        var longitude = element.longitude;
-        var cases = element.infected;
-        let color;
-        if (cases > 255) color = `rgb(255,0,0)`;
-        else color = `rgb(${cases},0,0)`;
+        let state_name = element.name;
+        let population = addCommas(element.pop);
+        let recovered = addCommas(element.recovered);
+        let sick = addCommas(element.sick);
+        let death = addCommas(element.dead);
+        let cases = addCommas(element.infected);
+        let latitude = element.latitude;
+        let longitude = element.longitude;
+        let information = `<h3>${state_name}</h3><p>Population = ${population}</p><p>Recovered = ${recovered}</p>
+        <p>Cases = ${cases}</p><p>Sick = ${sick}</p><p>Death = ${death}</p>`;
         new mapboxgl.Marker({
             draggable: false,
-            color: color
+            color: 'rgb(255,0,0)'
         })
             .setLngLat([longitude, latitude])
             .setPopup(
-                new mapboxgl.Popup({ offset: 25 }) // add popups
-                    .setHTML(
-                        '<h3>' +
-                        'Corona Cases ' +
-                        '</h3><p>' +
-                        'to Display ' +
-                        '</p>'
-                    )
+                new mapboxgl.Popup({ offset: 35 }) // add popups
+                    .setHTML(information)
             )
             .addTo(map);
     });
